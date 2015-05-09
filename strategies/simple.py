098a8353b6699d2298dc00d53532c79c5cafd42e
ArtermZhuk <zhuk1996@gmail.com>
2015-05-09 17:38:56 +0300

    added simple.py

--- strategies/simple.py
+++ strategies/simple.py
@@ -1,3 +1,16 @@
 from client import *
 
-print("I'm fine, bitch")
+"""for name in ["abc", "cde", "Surin", "Golov", "Arterm"]:
+	client.init()
+	client.join(name)
+	client.getField()
+	client.close()
+"""
+
+print(client.init())
+print(client.join("Vasya"))
+print(client.getField())
+sampleAction = {"owner" : "Vasya", "id" : "123", "position" : {"x" : 0, "y" : 1234}}
+print(client.moveUnit(sampleAction))
+print(client.attack(sampleAction))
+print(client.close())
