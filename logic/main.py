cefad82febec6a09baf1a0f4ebf6088b4821aabc
Golovanov399 <Golovanov12345@gmail.com>
2015-05-29 02:56:51 +0300

    Tried to generate new units

--- logic/main.py
+++ logic/main.py
@@ -92,10 +92,11 @@ def disconnect(action):
 
 def makeNewTurn():
 		global maxPlayersCount, Players
+		toDelete = []
 		for i in range(len(Players)):
 				if Players[i].base["health"] <= 0:	
 					stop(Players[i].listener, "Go to Hell")
-					del Players[i]
+					toDelete = [i] + toDelete
 					maxPlayersCount -= 1
 				else: 
 						uniqueId = 0
@@ -104,6 +105,8 @@ def makeNewTurn():
 								uniqueId += 1
 						Players[i].units.append(Unit(id = str(uniqueId), position = Players[i].base["position"], health = maxUnitHealth))
 						Players[i].base["health"] -= 1
+		for i in toDelete:
+				del Players[i]
 		if len(Players) == 0:
 			maxPlayersCount = 2
 		elif len(Players) == 1:
