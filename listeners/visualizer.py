0c47537346f6aec22ef7bf294f8a02927a5d1035
ArtermZhuk <zhuk1996@gmail.com>
2015-05-29 03:57:31 +0300

    visualiser beta again

--- listeners/visualizer.py
+++ listeners/visualizer.py
@@ -16,19 +16,24 @@ from structures.game import AttrDict
 
 def main(equeue, name, cpipe):
     def getField():
-        equeue.put((method, name, self))
+        equeue.put(('getField', '', name))
         return cpipe.recv()
 
     max_cnt = 300
+    fs = 300
+    out = 500
+    base_radius = 10
+    unit_radius = 3
 
+    colors = ["red", "blue", "green", "magenta"]
 
-    x, y = [0 for i in range(max_cnt)], [0 for i in range(max_cnt)]
+    x, y = [out for i in range(max_cnt)], [out for i in range(max_cnt)]
 
     output_server("circle_animate")
 
-    p = figure(x_range=[-300, 300], y_range=[-300, 300])
+    p = figure(x_range=[-fs, fs], y_range=[-fs, fs])
 
-    p.circle(x, y, radius=3, color="red", name="cir")
+    p.circle(x, y, radius=[3 for i in range(max_cnt)], color=["white" for i in range(max_cnt)], name="cir")
 
     show(p)
 
@@ -37,12 +42,34 @@ def main(equeue, name, cpipe):
 
     #print(ds.data)
 
-    cnt = 0
-
     while True:
-        x = [random.randint(-300, 300) for i in range(max_cnt)]
-        y = [random.randint(-300, 300) for i in range(max_cnt)]
+        x = [out for i in range(max_cnt)]
+        y = [out for i in range(max_cnt)]
+        c = ["white" for i in range(max_cnt)]
+        r = [1 for i in range(max_cnt)]
+        cnt = 0
+
+        def add(xs, ys, col, rad):
+            global x, y, c, r, cnt
+            x[cnt] = xs
+            y[cnt] = ys
+            c[cnt] = col
+            r[cnt] = rad
+            cnt += 1
+
+        field = getField()
+
+        for i in range(len(field)):
+            cur = field[i]
+            add(cur.base.position.x, cur.base.position.y, colors[i], 10)
+
+            for u in cur.units:
+                add(u.position.x, u.position.y, colors[i], 3)
+
+
         ds.data['x'] = x
         ds.data['y'] = y
+        ds.data['radius'] = r
+        ds.data['fill_color'] = c
         cursession().store_objects(ds)
         time.sleep(0.3)
