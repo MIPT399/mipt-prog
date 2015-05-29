816db001ca384607e00ae0ed2934a19765afa6e3
ArtermZhuk <zhuk1996@gmail.com>
2015-05-29 03:07:08 +0300

    visualizer beta

--- visualizer/visualizer.py
+++ visualizer/visualizer.py
@@ -1,14 +1,38 @@
-from bokeh.plotting import figure, output_file, show
+# The plot server must be running
+# Go to http://localhost:5006/bokeh to view this plot
 
-# output to static HTML file
-output_file("game.html")
+import time
 
-# create a new plot
-p = figure(
-   tools="pan,box_zoom,reset,save",
-   y_range=[0, 300],
-   x_range=[0, 300]
-)
+import random
 
-# show the results
+import numpy as np
+
+from bokeh.plotting import cursession, figure, show, output_server
+
+max_cnt = 300
+
+
+x, y = [0 for i in range(max_cnt)], [0 for i in range(max_cnt)]
+
+output_server("circle_animate")
+
+p = figure(x_range=[-300, 300], y_range=[-300, 300])
+
+p.circle(x, y, radius=3, color="red", name="cir")
+
 show(p)
+
+renderer = p.select(dict(name="cir"))
+ds = renderer[0].data_source
+
+#print(ds.data)
+
+cnt = 0
+
+while True:
+    x = [random.randint(-300, 300) for i in range(max_cnt)]
+    y = [random.randint(-300, 300) for i in range(max_cnt)]
+    ds.data['x'] = x
+    ds.data['y'] = y
+    cursession().store_objects(ds)
+    time.sleep(0.3)
